96585036812cf3d1944412ff13af90721f3ccdb8
siri_yu <yunus.sirin@dlr.de>
2024-05-02 18:01:30 +0200

    Implemented get-requst and added ToDo

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import geocoder
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from utils.dictFunctions import getAddressAndCoordinates
 
 # Initiating the router
 app = FastAPI()
 
-# TODO: Store sensible data in an .env file
+# App configs
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods or specify with ["POST", "GET"]
+    allow_headers=["*"],  # Allow all headers or specify with ["X-Custom-Header"]
+)
+
+# TODO: Store sensible data in an .env file or get the username from request body
 GEONAMES_USERNAME = 'siri_yu'
 
 @app.get('/api/coordinates')
